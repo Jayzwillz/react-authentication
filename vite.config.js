ec826065fabf12d0bfcfc2a34b2b76d60013b585
Jah'swill Emmanuel <jahswill4jahs@gmail.com>
2025-02-22 22:33:25 +0100

    remove unused dependencies and clean up configuration files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
